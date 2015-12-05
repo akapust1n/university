bb711453e28cc800710c2a19b52debacbf202c4f
djkah11 <djkah11@yandex.ru>
2015-12-05 22:50:05 +0300

    lab 10 40% is ready
    redy

--- lab10/main.c
+++ lab10/main.c
@@ -39,8 +39,7 @@ int menu1()
     printf("Ipnut function\n");
     printf("1)Input line in string\n");
     printf("2)Delete line from table\n");
-    printf("3)Change line\n");
-    printf("4)Variant task\n>>");
+    printf("3)Change line\n>>");
     scanf("%d",&key);
     return key;
 }
@@ -49,7 +48,8 @@ int menu2()
     int key;
     printf("Choose table\n");
     printf("1)Suppliers\n");
-    printf("2)Delete line from table\n>>");
+    printf("2)Delete line from table\n");
+    printf("3)Variant task\n>>");
     scanf("%d",&key);
     return key;
 }
@@ -137,7 +137,7 @@ int main()
                                      } while(1);
                                      City[y]='\0';
                                      strcpy(City,del(strtrim(City))); //удаление лишних символов
-                                    City[strlen(City)-1]='\0';
+                                    City[strlen(City)]='\0';  //а может и -1
                              }
 
                              if(feof(Suppliers)) break;
@@ -163,18 +163,178 @@ int main()
                         fclose(Suppliers);
                         printf("\nLine was added\n");
                      //вносим всё это в файл
-
+                         break;
                         //printf
                     }
+                    case 2:
+                      {
+                          int SupplierID1;
+                          int SupplierID;
+                          char  SupplierName[200];
+                          char  SupplierName1[200];
+                          int Status;
+                          char City[100];
+
+                          printf("Input SupplierID ");
+                          scanf("%d",&SupplierID1);
+                          FILE *f=fopen("temp.txt","w");
+                          Suppliers=fopen("Suppliers.txt","r");
+                          while (!feof(Suppliers))
+                          {
+                              fscanf(Suppliers,"%d",&SupplierID);
+
+                              int y=0;
+                              do
+                              {
+
+
+                                  fscanf(Suppliers,"%c",&h);
+                                  //if (h==' ') continue;
+                                   if(h>59||h<47) SupplierName[y++]=h;
+                                   else break;
+
+                              } while(1);
+                               SupplierName[y]='\0';
+                              strcpy(SupplierName,del(strtrim(SupplierName))); //удаление лишних символов
+                             SupplierName[strlen(SupplierName)-1]='\0';
+                                     // puts(SupplierName);
+
+                            if(h)  Status=h-'0';
+                              y=0;
+                              fscanf(Suppliers,"%c",&h);
+                                      if (h<58)
+                              {
+                                  Status*=10;
+                                  Status+=(h-'0');
+                              }
+                              else
+                              { City[y++]=h;}
+
+                                  do
+                                  {
+                                      fscanf(Suppliers,"%c",&h);
+                                       if(h!='\n'&&!feof(Suppliers)) City[y++]=h;
+                                       else break;
+
+                                  } while(1);
+                                  City[y]='\0';
+                                  strcpy(City,del(strtrim(City))); //удаление лишних символов
+                                 City[strlen(City)]='\0';
+                                 if (SupplierID==SupplierID1) continue;
+                                 fprintf(f,"\n%d    ",SupplierID);
+                                 fprintf(f,"%s ",SupplierName);
+                                 fprintf(f,"%d     ",Status);
+                                 fprintf(f,"%s    ",City);
+
+                          }
+                           fclose(Suppliers);
+                         fclose(f);
+                         system("del Suppliers.txt");
+                         system("rename temp.txt Suppliers.txt");
+                          printf("\nIf there was an element, element was deleted\n");
+                          break;
+                      }
+                    case 3:
+                     {
+                        int SupplierID1;
+                        int SupplierID;
+                        char  SupplierName[200];
+                        char  SupplierName1[200];
+                        int Status;
+                        char City[100];
+                        int flag=0;
+
+                        printf("Input SupplierID ");
+                        scanf("%d",&SupplierID1);
+                        FILE *f=fopen("temp.txt","w");
+                        Suppliers=fopen("Suppliers.txt","r");
+                        while (!feof(Suppliers))
+                        {
+                            fscanf(Suppliers,"%d",&SupplierID);
+                             if(SupplierID==SupplierID1) flag=1;
+                             else
+                                 flag=0;
+                            int y=0;
+                            do
+                            {
+
+
+                                fscanf(Suppliers,"%c",&h);
+
+                                //if (h==' ') continue;
+                                 if(h>59||h<47) SupplierName[y++]=h;
+                                 else break;
+
+                            } while(1);
+                             SupplierName[y]='\0';
+                            strcpy(SupplierName,del(strtrim(SupplierName))); //удаление лишних символов
+                           SupplierName[strlen(SupplierName)-1]='\0';
+                                   // puts(SupplierName);
+
+                          if(h)  Status=h-'0';
+                            y=0;
+                            fscanf(Suppliers,"%c",&h);
+                                    if (h<58)
+                            {
+                                Status*=10;
+                                Status+=(h-'0');
+                            }
+                            else
+                            { City[y++]=h;}
+
+                                do
+                                {
+                                    fscanf(Suppliers,"%c",&h);
+                                     if(h!='\n'&&!feof(Suppliers)) City[y++]=h;
+                                     else break;
+
+                                } while(1);
+                                City[y]='\0';
+                                strcpy(City,del(strtrim(City))); //удаление лишних символов
+                               City[strlen(City)]='\0';
+                               if (!flag) {
+                               fprintf(f,"\n%d    ",SupplierID);
+                               fprintf(f,"%s ",SupplierName);
+                               fprintf(f,"%d     ",Status);
+                               fprintf(f,"%s    ",City);
+                               }
+                               else
+                               {
+                                   printf("SupplierName ");
+                                   fflush(stdin);
+                                   gets(SupplierName);
+                                   do
+                                   {
+                                       printf("\nStatus ");
+                                       scanf("%d",&Status);
+                                   }while(!(Status>0&&Status<100));
+
+                                   printf("City ");
+                                   fflush(stdin);
+                                   gets(City);
+                                   fprintf(f,"\n%d    ",SupplierID);
+                                   fprintf(f,"%s ",SupplierName);
+                                   fprintf(f,"%d     ",Status);
+                                   fprintf(f,"%s    ",City);
+                               }
+
+
+                        }
+                         fclose(Suppliers);
+                       fclose(f);
+                       system("del Suppliers.txt");
+                       system("rename temp.txt Suppliers.txt");
+                        printf("\nIf there was an element, element was changed\n");
+                         break;
+                     }
 
                 }
                 break;
             }
-          case 2:
-            {
-                break;
-            }
+
+
         }
+
 
 
     }
